385ce5e33f0f8362ef0caf8a3b7f61076c219df0
Mickaël <60145073+MickaelMoon@users.noreply.github.com>
2022-03-03 15:40:34 +0100

    mm

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import Calculatrice from "./Components/Calculatrice/Calculatrice";
-
+import "./App.css";
 function App() {
   return (
     <>
+      <h1>Calculatrice React</h1>
       <Calculatrice />
     </>
   );
--- src/Components/Calculatrice/Calculatrice.js
+++ src/Components/Calculatrice/Calculatrice.js
@@ -1,40 +1,74 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Calculatrice.css";
 
 export default function Calculatrice() {
+  const [calc, setCalc] = useState("");
+  const [result, setResult] = useState("");
+
+  const ops = ["%", "/", "+", "-", "*", "."];
+
+  const updateCalc = (value) => {
+    if (
+      (ops.includes(value) && calc === "") ||
+      (ops.includes(value) && ops.includes(calc.slice(-1)))
+    ) {
+      return;
+    }
+    setCalc(calc + value);
+
+    if (!ops.includes(value)) {
+      setResult(eval(calc + value).toString());
+    }
+  };
+  const createDigits = () => {
+    const digits = [];
+
+    for (let i = 1; i < 10; i++) {
+      digits.push(
+        <button key={i} onClick={() => updateCalc(i.toString())}>
+          {i}
+        </button>
+      );
+    }
+    return digits;
+  };
+  const calculate = () => {
+    setCalc(eval(calc).toString());
+  };
+  const deleteLast = () => {
+    if (calc == "") {
+      return;
+    }
+    const value = calc.slice(0, -1);
+    setCalc(value);
+    if (ops.includes(value.slice(-1))) {
+      setResult(eval(value.toString().slice(0, -1)));
+    } else {
+      setResult(eval(value.toString()));
+    }
+  };
   return (
-    <div className="container-calculatrice">
-      <div className="display">
-        <span>(0)</span>0
-      </div>
-      <div className="component-clavier">
-        <div className="first-row">
-          <button className="btn-clear">DEL</button>
-          <button>%</button>
-          <button>/</button>
+    <div className="App">
+      <div className="calculator">
+        <div className="display">
+          {result ? <span>({result})</span> : ""}
+          {calc || "0"}
         </div>
-        <div className="second-row">
-          <button>7</button>
-          <button>8</button>
-          <button>9</button>
-          <button>x</button>
-        </div>
-        <div className="third-row">
-          <button>4</button>
-          <button>5</button>
-          <button>6</button>
-          <button>-</button>
-        </div>
-        <div className="fourth-row">
-          <button>1</button>
-          <button>2</button>
-          <button>3</button>
-          <button>+</button>
-        </div>
-        <div className="last-row">
-          <button className="btn-zero">0</button>
-          <button>.</button>
-          <button>=</button>
+        <div className="component-clavier">
+          <div className="operators">
+            <button onClick={deleteLast}>DEL</button>
+            <button onClick={() => updateCalc("%")}>%</button>
+            <button onClick={() => updateCalc("/")}>/</button>
+            <button onClick={() => updateCalc("*")}>x</button>
+            <button onClick={() => updateCalc("-")}>-</button>
+            <button onClick={() => updateCalc("+")}>+</button>
+          </div>
+          <div className="digits">
+            {createDigits()}
+            <button onClick={() => updateCalc("0")}>0</button>
+            <button onClick={() => updateCalc(".")}>.</button>
+            <button onClick={calculate}>=</button>
+          </div>
         </div>
       </div>
     </div>
